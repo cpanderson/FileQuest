e25d67c1eb57eb5a3af76978a06ebdbb15d37b15
Chris Anderson <cpanderson@me.com>
2010-02-22 12:14:09 -0800

    initial commit

---
+++ lib/filequest.rb
@@ -0,0 +1,71 @@
+# FileQuest
+
+# This plugin accesses Spotlight indexes on OS X and locate indexes on linux. It assumes that the user 
+# the rails server is running as has access to the indexes specified when initializing FileQuest and also
+# that Spotlight and locate are functioning normally.
+# 
+# Usage: FileQuest::Search.new(<directory path for search>, <search term>, <options>)
+#
+# Example: @files = FileQuest::Search.new("/Shared/Public", "environment", {:name_only => true, :doc => true, :pdf => true})  
+# 
+# Options:
+#          :name      => (true||false)              search for file name only, not contents (Spotlight only)
+#          :doc       => (true||false)              search for documents
+#          :xls       => (true||false)              search for spreadsheets
+#          :pdf       => (true||false)              search for pdfs
+#          :all       => (true||false)              search for docs, spreadsheets and pdfs
+
+module FileQuest
+  
+  class Search
+    
+    def initialize(directory, query, options = {})
+      @dir = directory
+      @query = query
+      options.assert_valid_keys(:name, :doc, :xls, :pdf, :all)
+      @options = options
+    end
+    
+    def dir
+      @dir
+    end
+    
+    def query
+      @query
+    end
+    
+    def options
+      @options
+    end
+    
+    def command
+      if RUBY_PLATFORM =~ /darwin/
+        @command = "mdfind -onlyin #{dir}"
+        @command += ' -name "' + query + '"' if @options[:name]
+        @command += " " + query if ! @options[:name_only]
+      elsif RUBY_PLATFORM =~ /linux/
+        #@command = 'locate ' + query + " | grep " + dir
+        @command = 'locate -d /mnt/merserv/var/lib/slocate/slocate.db ' + query + " | grep -iE '" + dir + ".*\.(doc|pdf|xls)'"
+      end
+      @command
+    end
+    
+    def search
+      command
+      results = %x[#{@command}]
+      results.split(/\n/)
+    end
+    
+    private
+    
+    def spotlight
+      
+    end
+    
+    def slocate
+      
+    end
+    
+  end
+  
+end
---
+++ tasks/file_quest_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :file_quest do
+#   # Task goes here
+# end
